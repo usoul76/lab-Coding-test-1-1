4cec808c81739d195609f5fba08ce1c3fac14d8c
usoul76 <109453395+usoul76@users.noreply.github.com>
2026-03-27 13:28:29 +0900

    짝수와홀수 문제풀

---
+++ 짝수와홀수.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(int num) {
+        String answer = "";
+        if(num % 2 == 0){
+            return "Even";
+        }else{
+            return "Odd";
+        }
+    }
+}
+//문제링크 : https://school.programmers.co.kr/learn/courses/30/lessons/12937
