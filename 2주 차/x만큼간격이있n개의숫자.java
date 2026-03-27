709794dc8c7b91a88a95fd995630711f88609b8c
usoul76 <109453395+usoul76@users.noreply.github.com>
2026-03-27 13:32:11 +0900

    Create x만큼간격이있n개의숫자.java

---
+++ 2주 차/x만큼간격이있n개의숫자.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        for(int i = 0; i < n; i++){
+            answer[i] = (long)x *(i+1);
+        }
+        return answer;
+    }
+}
+//문제링크 : https://school.programmers.co.kr/learn/courses/30/lessons/12954
