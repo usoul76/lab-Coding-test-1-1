d4726b0de7a988f3c5f5a2734ef502a9585e1823
usoul76 <109453395+usoul76@users.noreply.github.com>
2026-04-03 14:52:04 +0900

    Add problem link comment to 문자열을정수로바꾸기.java

---
+++ 3주 차/문자열을정수로바꾸기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(String s) {
+        int answer = Integer.parseInt(s);
+        return answer;
+    }
+}
+//문제링크 : https://school.programmers.co.kr/learn/courses/30/lessons/12925
