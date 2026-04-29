5779d4d0b37f30f0d93b715bb49547b1fe251b44
usoul76 <109453395+usoul76@users.noreply.github.com>
2026-04-29 13:42:09 +0900

    Implement solution to reverse digits of a number

---
+++ 5주 차/자연수뒤집어배열만들기.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(long n) {
+        String s  = String.valueOf(n);
+        int[] answer = new int[s.length()];
+        
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = Integer.parseInt(s.substring(answer.length-1-i, answer.length-i));
+        }
+        
+        return answer;
+    }
+}
+//문제링크 : https://school.programmers.co.kr/learn/courses/30/lessons/12932#
