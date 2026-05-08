21464d110b88cb739b639d9e0cf2738a55dac492
usoul76 <109453395+usoul76@users.noreply.github.com>
2026-05-08 09:52:02 +0900

    Create 정수제곱근판별.java

---
+++ 6주 차/정수제곱근판별.java
@@ -0,0 +1,14 @@
+class Solution {
+  public long solution(long n) {
+      if(n == 1){
+          return 4;
+      }
+      for(long i = 2; i < n; i++){
+          if(n / i == i && n % i ==0){
+              return (i + 1) * (i + 1);
+          }
+      }
+      return - 1;
+  }
+}
+//문제링크 : https://school.programmers.co.kr/learn/courses/30/lessons/12934
