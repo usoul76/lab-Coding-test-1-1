8979601699bc7954d52f53af4539086f6b3f98f7
usoul76 <109453395+usoul76@users.noreply.github.com>
2026-05-15 15:52:56 +0900

    Create 나머지가1이되는수.java

---
+++ 7주 차/나머지가1이되는수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 2; i < n; i++){
+            if(n % i == 1){
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
+//문제링크 : https://school.programmers.co.kr/learn/courses/30/lessons/87389
