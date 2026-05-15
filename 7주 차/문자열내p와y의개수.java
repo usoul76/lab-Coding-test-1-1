8636b9e41db884b42b1259a6917fdbe2b3e69d8b
usoul76 <109453395+usoul76@users.noreply.github.com>
2026-05-15 15:51:58 +0900

    Create 문자열내p와y의개수.java

---
+++ 7주 차/문자열내p와y의개수.java
@@ -0,0 +1,18 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        int count = 0;
+        s = s.toLowerCase();
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i)=='p') count++;
+            else if(s.charAt(i)=='y') count--;
+        }
+        if(count!=0) answer = false;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
